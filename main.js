56a88985eec21f1cde262423cb91076f752dcc2a
pby <pby2017@github.com>
2017-12-06 03:02:08 +0900

    171206

--- main.js
+++ main.js
@@ -22,3 +22,26 @@ function cantUse()
 {
   alert("Sorry. You can not use now.")
 }
+
+function searchinput()
+{
+  var searchtext=prompt('What is your text for search image? This open new windows');
+  if(searchtext==null || searchtext=="")
+  {
+    return false;
+  }
+  var getidsearchanchor = document.getElementById('searchimageanchor');
+  getidsearchanchor.href="https://www.google.co.kr/search?q="+searchtext+"&dcr=0&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiIxN6FrvPXAhWIErwKHYe3DdUQ_AUICigB&biw=1366&bih=637";
+}
+
+function changetmpimg()
+{
+  var tmpimglogintree = document.getElementById('tmpimglogintree');
+  tmpimglogintree.src="https://cdn.pixabay.com/photo/2017/10/28/12/57/winter-2896970_960_720.jpg";
+}
+
+function returntmpimg()
+{
+  var tmpimglogintree = document.getElementById('tmpimglogintree');
+  tmpimglogintree.src="http://cafefiles.naver.net/20101218_191/vice1210_1292643986876TwNLz_jpg/21_vice1210.jpg";
+}
