7b03a19572fbc1da9a07dfa1004022170ec704cb
ByungYongPark <qkrquddyd94@naver.com>
2017-12-08 17:34:09 +0900

    add comment in main.js

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 var checkID="";
+// VALUE. signin.html. 아이디 중복확인할 때 아이디 저장할 변수.
 var sameCheckBool=false;
-
+// VALUE. signin.html. 아이디 중복확인 변수.
+/*VALUE.
+------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------*/
+// COMMON. main.html으로 이동.
+function gotomainhtml()
+{
+  location.href="main.html";
+}
+// COMMON. signun으로 이동.
+function gotosigninhtml()
+{
+  location.href="signin.html";
+}
+/*COMMON.
+------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------*/
+// main.html. 링크 노출을 숨기기 위해 init 함수로 이미지 출력.
 function init()
 {
   var mainlogo = document.getElementById('mainlogo');
@@ -9,7 +27,83 @@ function init()
   var tmpimglogintree = document.getElementById('tmpimglogintree');
   tmpimglogintree.src="http://cafefiles.naver.net/20101218_191/vice1210_1292643986876TwNLz_jpg/21_vice1210.jpg";
 }
-
+// main.html. 시작화면 클릭시 시작화면 사라지고, 시간이 보이도록 작동.
+function hidestartviewimg()
+{
+  var startviewimg = document.getElementById('startviewimg');
+  startviewimg.src="";
+  startviewimg.alt="";
+  var realtimeview = document.getElementById('realtimeview');
+  setInterval(function(){
+    realtimeview.innerHTML = realtime()
+  }, 1000);
+}
+// main.html. 1번째 메뉴의 기능으로 시작화면을 보여줌.
+function showstartview()
+{
+  var startviewanchor = document.getElementById('startviewanchor');
+  startviewanchor.href = "startview.html";
+}
+// main.html. 2번째 메뉴의 기능으로 게임 사이트의 목록을 보여줌.
+function showgamesitelistview()
+{
+  var gamesitelistanchor = document.getElementById('gamesitelistanchor');
+  gamesitelistanchor.href = "gamesitelist.html";
+}
+// main.html. 3번째 메뉴의 기능으로 코딩 사이트의 목록을 보여줌.
+function showpssitelistview()
+{
+  var pssitelistanchor = document.getElementById('pssitelistanchor');
+  pssitelistanchor.href = "pssitelist.html";
+}
+// main.html. 4번째 메뉴의 기능으로 구글 이미지 검색, 태그의 id값이 겹쳐서 get id에서 null 반환되는 문제 있었음.
+function searchinput()
+{
+  var searchtext=prompt('What is your text for search image? This open new windows');
+  if(searchtext==null || searchtext=="")
+  {
+    return false;
+  }
+  var getidsearchanchor = document.getElementById('searchimageanchor');
+  getidsearchanchor.href="https://www.google.co.kr/search?q="+searchtext+"&dcr=0&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiIxN6FrvPXAhWIErwKHYe3DdUQ_AUICigB&biw=1366&bih=637";
+}
+// main.html. 5번째 메뉴의 기능으로 내 블로그를 보여줌.
+function showmyblogview()
+{
+  var mybloganchor = document.getElementById('mybloganchor');
+  mybloganchor.href = "https://blog.naver.com/qkrquddyd94";
+}
+// main.html의 로그인 버튼 아래 이미지에 마우스 올릴 때 이미지 변경.
+function changetmpimg()
+{
+  var tmpimglogintree = document.getElementById('tmpimglogintree');
+  tmpimglogintree.src="https://cdn.pixabay.com/photo/2017/10/28/12/57/winter-2896970_960_720.jpg";
+}
+// main.html. onmouseover로 변환된 이미지에서 onmouseout할 때 다시 원래 이미지로 복구 되는 메소드.
+function returntmpimg()
+{
+  var tmpimglogintree = document.getElementById('tmpimglogintree');
+  tmpimglogintree.src="http://cafefiles.naver.net/20101218_191/vice1210_1292643986876TwNLz_jpg/21_vice1210.jpg";
+}
+// main.html 로그인 버튼의 기능.
+function signinbutton()
+{
+  location.href="signin.html";
+}
+// main.html. 시작화면을 클릭했을 때 실제 시간을 보여주기 위한 메소드.
+function realtime()
+{
+  var realtime = new Date();
+  var hours = realtime.getHours();
+  var minutes = realtime.getMinutes();
+  var seconds = realtime.getSeconds();
+  var writetime = hours+"시 "+minutes+"분 "+seconds+"초";
+  return writetime;
+}
+/*main.html
+------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------*/
+// signin.html. 로그인할 때 아이디와 비밀번호 입력했는지 확인 후 값 전송.
 function signincheck()
 {
   if(document.signin.idmain.value=="")
@@ -30,55 +124,20 @@ function signincheck()
     window.href="main.html";
   }
 }
-
+// signin.html. 사용 못하는 기능 : find ID/PW.
 function cantUse()
 {
   alert("Sorry. You can not use now.")
 }
-
-function searchinput()
-{
-  var searchtext=prompt('What is your text for search image? This open new windows');
-  if(searchtext==null || searchtext=="")
-  {
-    return false;
-  }
-  var getidsearchanchor = document.getElementById('searchimageanchor');
-  getidsearchanchor.href="https://www.google.co.kr/search?q="+searchtext+"&dcr=0&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiIxN6FrvPXAhWIErwKHYe3DdUQ_AUICigB&biw=1366&bih=637";
-} // 태그의 id값이 겹쳐서 get id에서 null 반환되는 문제 있었음.
-
-function changetmpimg()
-{
-  var tmpimglogintree = document.getElementById('tmpimglogintree');
-  tmpimglogintree.src="https://cdn.pixabay.com/photo/2017/10/28/12/57/winter-2896970_960_720.jpg";
-}
-
-function returntmpimg()
-{
-  var tmpimglogintree = document.getElementById('tmpimglogintree');
-  tmpimglogintree.src="http://cafefiles.naver.net/20101218_191/vice1210_1292643986876TwNLz_jpg/21_vice1210.jpg";
-}
-
-function signinbutton()
-{
-  location.href="signin.html";
-}
-
-function realtime()
-{
-  var realtime = new Date();
-  var hours = realtime.getHours();
-  var minutes = realtime.getMinutes();
-  var seconds = realtime.getSeconds();
-  var writetime = hours+"시 "+minutes+"분 "+seconds+"초";
-  return writetime;
-}
-
+// signin.html. 회원가입 버튼의 기능.
 function signupbutton()
 {
   location.href="signup.html";
 }
-
+/*signin.html
+------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------*/
+// signup.html. 회원가입 확인.
 function signupcheck()
 {
   if(document.signup.idinput.value=="")
@@ -168,7 +227,7 @@ function signupcheck()
   window.href="signin.html";
   sameCheckBool = false;
 }
-
+// signup.html. 회원가입할 때, 아이디 중복 확인.
 function clicksamecheck()
 {
   if(document.signup.idinput.value!="")
@@ -184,48 +243,6 @@ function clicksamecheck()
     return false;
   }
 }
-
-function gotomainhtml()
-{
-  location.href="main.html";
-}
-
-function gotosigninhtml()
-{
-  location.href="signin.html";
-}
-
-function showstartview()
-{
-  var startviewanchor = document.getElementById('startviewanchor');
-  startviewanchor.href = "startview.html";
-}
-
-function hidestartviewimg()
-{
-  var startviewimg = document.getElementById('startviewimg');
-  startviewimg.src="";
-  startviewimg.alt="";
-  var realtimeview = document.getElementById('realtimeview');
-  setInterval(function(){
-    realtimeview.innerHTML = realtime()
-  }, 1000);
-}
-
-function showgamesitelistview()
-{
-  var gamesitelistanchor = document.getElementById('gamesitelistanchor');
-  gamesitelistanchor.href = "gamesitelist.html";
-}
-
-function showpssitelistview()
-{
-  var pssitelistanchor = document.getElementById('pssitelistanchor');
-  pssitelistanchor.href = "pssitelist.html";
-}
-
-function showmyblogview()
-{
-  var mybloganchor = document.getElementById('mybloganchor');
-  mybloganchor.href = "https://blog.naver.com/qkrquddyd94";
-}
+/*signup.html
+------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------*/
